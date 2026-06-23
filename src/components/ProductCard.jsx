b4d977f81fae5d2981b684dcc45f09acff0ee204
SabaaSandhu <sabaasandhu789@gmail.com>
2026-06-22 18:09:52 -0700

    Fix ProductCard image URL with full path

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -79,14 +79,12 @@ const ProductCard = ({ product, index }) => {
 
         <div className="w-full h-50 flex items-center justify-center">
           <img
-            src={
-              isHover
-                ? `${apis[2]}${product.images?.[0]?.image || product.images?.[0]?.image}`
-                : `${apis[2]}${product.images?.[1]?.image}`
-
-                // ? `${apis[2]}${product.images[0].image}`
-                // : `${apis[2]}${product.images[1].image}`
-            }
+            
+           src={
+      product.images && product.images.length > 0
+        ? `https://django-production-126c.up.railway.app${product.images[0].image}`
+        : '/placeholder.jpg'
+    }
             alt={name}
             className={`w-full h-80 mt-5 object-contain transition-transform duration-300 ${
               isHover ? "scale-105" : "scale-100"
