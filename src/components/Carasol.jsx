d574274847b886681714bd724635b0b1f8f4dd85
SabaaSandhu <sabaasandhu789@gmail.com>
2026-06-24 17:49:44 -0700

    Fix Carasol image

--- src/components/Carasol.jsx
+++ src/components/Carasol.jsx
@@ -1,22 +1,29 @@
 import React, { useCallback, useEffect } from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import AutoScroll from "embla-carousel-auto-scroll";
-import video from "../image/video.mp4"
 
 const Carasol = ({ category }) => {
   let slides = [];
 
   if (category === "khadar") {
-   slides = [
-       { type: 'video', src: video }
+    slides = [
+      "https://hub.wtm.com/wp-content/uploads/2018/08/Blogging.jpg",
+      "https://image/cc1.webp",
+      "https://cdn.shopify.com/s/files/1/0248/7892/t/52/assets/hamilton-watches-banner_1440x.jpg",
+      "https://i.pinimg.com/originals/4f/7f/83/4f7f83f20552d1055c2f7f4a3d84b6bf.jpg",
     ];
   } else if (category === "valvet") {
-   slides = [
-       { type: 'video', src: video }
+    slides = [
+      "https://hub.wtm.com/wp-content/uploads/2018/08/Blogging.jpg",
+      "https://image/cc1.webp",
+      "https://cdn.shopify.com/s/files/1/0248/7892/t/52/assets/hamilton-watches-banner_1440x.jpg",
+      "https://i.pinimg.com/originals/4f/7f/83/4f7f83f20552d1055c2f7f4a3d84b6bf.jpg",
     ];
   } else {
     slides = [
-       { type: 'video', src: video }
+      "https://styka.pk/cdn/shop/files/Styka-01.webp?v=1764914779&width=1370",
+      "https://styka.pk/cdn/shop/files/Styka-03.webp?v=1764914779&width=1370",
+      "https://www.mariab.pk/cdn/shop/files/Luxury_Pret_Web_Banner_02.jpg?v=1763444782",
     ];
   }
 
@@ -35,23 +42,13 @@ const Carasol = ({ category }) => {
   return (
     <div className="embla overflow-hidden" ref={emblaRef}>
       <div className="embla__container flex">
-        {slides.map((slide, i) => (
+        {slides.map((img, i) => (
           <div className="embla__slide min-w-full" key={i}>
-            {slide.type === 'video' ? (
-              <video
-                src={slide.src}
-                className="w-full h-[50px] md:h-[50px] object-cover rounded-xl"
-                alt=""
-                autoPlay
-                loop
-              />
-            ) : (
-              <img
-                src={slide.src}
-                className="w-full h-[50px] md:h-[50px] object-cover rounded-xl"
-                alt=""
-              />
-            )}
+            <img
+              src={img}
+              className="w-full h-[250px] md:h-[450px] object-cover rounded-xl"
+              alt=""
+            />
           </div>
         ))}
       </div>
