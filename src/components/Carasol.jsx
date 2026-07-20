977877dc6277ee4953405c4be2f5ebacca85116a
SabaaSandhu <sabaasandhu789@gmail.com>
2026-07-20 16:26:13 -0700

    carasol

--- src/components/Carasol.jsx
+++ src/components/Carasol.jsx
@@ -3,7 +3,7 @@ import useEmblaCarousel from "embla-carousel-react";
 import AutoScroll from "embla-carousel-auto-scroll";
 import w from "../image/w.jpg";
 import r from "../image/r.png";
-import video from "../image/video.mp4";
+import v from "../image/v.png";
 
 const Carasol = ({ category }) => {
   let slides = [];
@@ -24,7 +24,7 @@ const Carasol = ({ category }) => {
     ];
   } else {
     slides = [
-       video,
+       v,
        r,
        w,
       
